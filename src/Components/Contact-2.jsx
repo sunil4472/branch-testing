6c455861226eec45d9432c6ebfb82f1d2b941376
Sunil Neupane <01.princesunil@gmail.com>
2025-08-05 10:45:02 +0545

    create and testing new branch

---
+++ src/Components/Contact-2.jsx
@@ -0,0 +1,8 @@
+const Contact2 = () => {
+  return (
+    <div>
+      <h1>just branching testing okay</h1>
+    </div>
+  );
+};
+export default Contact2;
--- src/Pages/contact.jsx
+++ src/Pages/contact.jsx
@@ -1,9 +1,11 @@
 import Contact from "../Components/Contact";
+import Contact2 from "../Components/Contact-2";
 
 const ContactPage = () => {
   return (
     <div className="contactWrapper">
       <Contact />
+      <Contact2 />
     </div>
   );
 };
